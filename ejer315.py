65ceda6834523933f1bb662cc1715dfec998140d
PabloOlveraColino <polvcol404@g.educaand.es>
2024-10-21 12:26:55 +0200

    Add files via upload

---
+++ ejer315.py
@@ -0,0 +1,16 @@
+#Ejercicio 3.1.5
+
+#Escribir un programa que almacene en una lista los números del 1 al 10 
+# y los muestre por pantalla en orden inverso separados por comas.
+
+#def Visualizar(lista):
+#    lista.reverse()
+#    print(*lista,sep=", ")
+
+def inverso_lista(lista):
+    return lista.reverse()
+
+
+lista = [1,2,3,4,5,6,6,7,8,9,10]
+#Visualizar(lista)
+
---
+++ test_ejer315.py
@@ -0,0 +1,7 @@
+from ejer315 import *
+
+# hace un test de una lista usando el programa de reverse de ejer315.py
+def test_inverso_lista():
+    lista=[1,2,3]
+    inversa=lista.reverse()
+    assert inverso_lista(lista) == inversa
